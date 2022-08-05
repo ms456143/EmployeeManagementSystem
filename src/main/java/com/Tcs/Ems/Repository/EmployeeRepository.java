1d9a1a996aa5220e5f296f8aa4fbc4b63d82acfe
ms456143 <ms456143@gmail.com>
2022-08-05 13:04:05 -0700

    ok

--- src/main/java/com/Tcs/Ems/Repository/EmployeeRepository.java
+++ src/main/java/com/Tcs/Ems/Repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.Tcs.Ems.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Tcs.Ems.Model.EmployeeEntity;
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer>{
 
+	public EmployeeEntity findByEmpNameAndEmpEmail(String name,String email);
 }
--- src/main/java/com/Tcs/Ems/Service/EmployeeServiceImpl.java
+++ src/main/java/com/Tcs/Ems/Service/EmployeeServiceImpl.java
@@ -1,5 +1,21 @@
 package com.Tcs.Ems.Service;
 
-public class EmployeeServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Tcs.Ems.Model.EmployeeEntity;
+import com.Tcs.Ems.Repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeServiceI{
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public EmployeeEntity login(String name, String email) {
+		EmployeeEntity log = employeeRepository.findByEmpNameAndEmpEmail(name, email);
+		return log;
+	}
 
 }
