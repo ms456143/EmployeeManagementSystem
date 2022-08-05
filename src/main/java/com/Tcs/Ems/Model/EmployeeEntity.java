49665e5dc42ea48c7439b1949039299cbe90cd98
ms456143 <ms456143@gmail.com>
2022-08-05 12:51:11 -0700

    entity

--- src/main/java/com/Tcs/Ems/Model/EmployeeEntity.java
+++ src/main/java/com/Tcs/Ems/Model/EmployeeEntity.java
@@ -1,7 +1,15 @@
 package com.Tcs.Ems.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class EmployeeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer empId;
 	private String empName;
 	private String empAddress;
